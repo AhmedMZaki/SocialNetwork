708721ebd94377b0496b73006b3adaee370f5fa1
Ahmed <ahmedmzaki22@gmail.com>
2017-12-01 18:54:51 +0200

    creating user table and controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
 
 @section('content')
   <div class="row well">
+    {{--  sign in form --}}
     <div class="col-md-6">
+      <h3>Sign In</h3>
        <form action="#" method="post">
            <div class="form-group">
              <label for="email">Your E-Mail</label>
@@ -19,15 +21,18 @@
            <button type="submit" name="submit" class="btn btn-primary">Sign In</button>
        </form>
     </div>
+
     <div class="col-md-6">
+       {{-- registration form --}}
+      <h3>Sign Up</h3>
        <form action="#" method="post">
+         <div class="form-group">
+           <label for="first_name">First Name</label>
+           <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name">
+         </div>
            <div class="form-group">
              <label for="email">Your E-Mail</label>
              <input type="email" class="form-control" name="email" id="email" placeholder="your email">
-           </div>
-           <div class="form-group">
-             <label for="first_name">First Name</label>
-             <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name">
            </div>
            <div class="form-group">
              <label for="password">Password</label>
